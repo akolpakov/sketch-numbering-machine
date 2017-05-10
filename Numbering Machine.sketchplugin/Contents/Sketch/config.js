3906dc3f9087122624dc0da498de1b50391b0396
Andrey Kolpakov <aakolpakov@gmail.com>
2017-05-10 16:21:57 +0200

    shifo generator

--- Numbering Machine.sketchplugin/Contents/Sketch/config.js
+++ Numbering Machine.sketchplugin/Contents/Sketch/config.js
@@ -21,14 +21,16 @@ var NUMBER_DIRECTIONS = [NUMBER_DIRECTION_DIRECT, NUMBER_DIRECTION_DRILL];
 
 // Defaults
 
-var SETTINGS_TEMPLATE = '0001-[*]';
+var SETTINGS_TEMPLATE = '[*]';
 var SETTINGS_PAD_STRING = '0';
 var SETTINGS_ALGORITHM = null;
-var SETTINGS_PAD_SIZE = 4;
+var SETTINGS_PAD_SIZE = 6;
 
-var SETTINGS_NUMBER_FROM = 1;
+var SETTINGS_NUMBER_FROM = 100;
 var SETTINGS_NUMBER_STEP = 1;
 var SETTINGS_NUMBER_AMOUNT = 10;
 var SETTINGS_NUMBER_DIRECTION = NUMBER_DIRECTION_DIRECT;
 
-var CUSTOM_NUMBER_PROCESSING = null;   // Could place custom function to process number
+var CUSTOM_NUMBER_PROCESSING = function(number) {
+    return number.substring(0, 3) + '-' + number.substring(3, 6);
+};
